307d76e44592d7e84bfc8a574462c955ea055042
PJ Herring <herring.pj@gmail.com>
2016-02-24 22:19:20 -0500

    change whole background color on tap

--- assets/components/instruments/module.js
+++ assets/components/instruments/module.js
@@ -2,6 +2,15 @@
 
 (function(module) {
 
+    var backgroundColor = [
+        '#6C62FE', '#7959E5', '#8751CD', '#9549B4', '#A3409C', '#B13883',
+        '#BE306B', '#CC2752', '#DA1F3A', '#E81721', '#F60F09', '#F62611',
+        '#F73E1A', '#F85623', '#F96E26', '#FA8635', '#FA9D3E', '#FBB547',
+        '#FCCD50', '#FDE559', '#FEFD62', '#E4F858', '#CBF34E', '#B1EE44',
+        '#98E93A', '#7FE531', '#65E027', '#4CDB1D', '#32D613', '#19D109',
+        '#00CD00'
+    ];
+
     function InstrumentsController($scope, _, $location, sounds) {
 
         $scope.sounds = sounds;
@@ -34,6 +43,8 @@
             }
             audio.audio.onended = function() {
                 audio.playing = false;
+                $scope.worshipBg = {};
+                $scope.worshipButton = {};
                 $scope.$apply();
             };
             audio.audio.onplay = function() {
@@ -44,8 +55,14 @@
 
         $scope.sounds = instrument.audio;
 
-        $scope.play = function(sound) {
+        $scope.play = function(sound, idx) {
             sound.audio.play();
+            $scope.worshipBg = {
+                'background-color': backgroundColor[idx]
+            };
+            $scope.worshipButton = {
+                'background-color': backgroundColor[idx]
+            };
         }
 
         $scope.goBack = function() {
